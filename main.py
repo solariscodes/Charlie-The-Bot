2da43ede986e366427f2c291a0ece08f989f14d5
Rodrigo Solaris <rodrigo@wicked.art.br>
2021-03-08 03:49:18 +0000

    Working

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 '''
 Simple Discord bot written in Python
-It returns a random Chuck Norris's phrase when someone gets online
+It returns a random Chuck Norris's quote when someone gets online
 by Solriscodes
 07/03/2021
 '''
@@ -8,15 +8,21 @@ import discord
 import os
 import json
 import requests
-
 import logging
+from server import keep_alive
 
+'''
+saving logs on discord.log
+'''
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
+'''
+get a random quote from Chuck Norris API
+'''
 def Norris():
   api = "https://api.chucknorris.io/jokes/random"
   req = requests.get(api)
@@ -27,6 +33,9 @@ intents = discord.Intents.all()
 intents.members = True
 client = discord.Client(intents=intents)
 
+'''
+connect to discord
+'''
 class Bot():
   @client.event
   async def on_ready():
@@ -35,10 +44,11 @@ class Bot():
   async def on_message(message):    
       if message.author == client.user:
         return
-      if message.content.startswith('entrou'):
+      if message.content.startswith('hello'):
         await message.channel.send("Hello back!")
   @client.event
   async def on_member_update(before, after):
-    await before.send(Norris())            
-  client.run(os.getenv('TOKEN'))    
+    await before.send(Norris())
+  keep_alive()            
+  client.run(os.getenv('TOKEN'))   
 Bot()
