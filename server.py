a93c2541fb28484a270d3b62d17f015b6f739b6d
Rodrigo Solaris <78048715+solariscodes@users.noreply.github.com>
2021-03-30 21:17:45 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from threading import Thread
 
+porta = int(os.environ.get('PORT', 5000))
+
 app = Flask('')
 
 @app.route('/')
@@ -8,7 +10,7 @@ def home():
     return "Hi! How are you?"
 
 def run():
-  app.run(host='0.0.0.0',port=80)
+  app.run(host='0.0.0.0',port=porta)
 def keep_alive():
     t = Thread(target=run)
     t.start()
